710dc7dc3d34345f7af8091d174097324a6eaae6
Alexander Rykov <154324-alexander-rykov@users.noreply.gitlab.com>
2022-09-11 13:28:24 +0300

    Fix task comment - replace null with -1.

--- NextBigger/NumberExtension.cs
+++ NextBigger/NumberExtension.cs
@@ -3,11 +3,11 @@
     public static class NumberExtension
     {
         /// <summary>
-        /// Finds the nearest largest integer consisting of the digits of the given positive integer number and null if no such number exists.
+        /// Finds the nearest largest integer consisting of the digits of the given positive integer number; return -1 if no such number exists.
         /// </summary>
         /// <param name="number">Source number.</param>
         /// <returns>
-        /// The nearest largest integer consisting of the digits  of the given positive integer and null if no such number exists.
+        /// The nearest largest integer consisting of the digits  of the given positive integer; return -1 if no such number exists.
         /// </returns>
         /// <exception cref="ArgumentException">Thrown when source number is less than 0.</exception>
         public static int NextBiggerThan(int number)
